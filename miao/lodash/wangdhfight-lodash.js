0675f23c352b1c6db630a996ed1304bc5c5c33ab
王丁辉 <1534212@qq.com>
2020-12-12 20:48:01 +0800

    提交

--- miao/lodash/wangdhfight-lodash.js
+++ miao/lodash/wangdhfight-lodash.js
@@ -1,36 +1,113 @@
     var wangdhfight = function(){
-        function chunk(ary,size) {
+        function chunk(array,size) {
             var result = []
-            if(size > ary.length){
-                result = ary
+            if(size > array.length){
+                result = array
             }else{
                 var result1 = []
                 var result2 = []
                 for(var i = 0;i < size;i++){
-                    result1.push(ary[i])
+                    result1.push(array[i])
                 }
-                for(var i = size;i < ary.length; i++){
-                    result2.push(ary[i])
+                for(var i = size;i < array.length; i++){
+                    result2.push(array[i])
                 }
                 result = [result1 , result2]
             }
             return result
         }
-        function compact(ary){
+
+
+
+        function compact(array){
             var result = []
-            for(var i=0;i <= ary.length-1;i++){
+            for(var i = 0 ; i <= array.length-1 ; i++){
                 if(ary[i]){
-                    result.push(ary[i])
+                    result.push(array[i])
                 }
             }
             return result
         }
-        function concat(ary){
-            var result = []
-            for(var i=0;i<ary.length)
+
+        function concat(array,...args){
+            var result = array
+            for(var i = 0;i < args.length;i++){
+                if(Array.isArray(args[i])){
+                    result.push(...args[i])
+                }else{
+                    result.push(args[i])
+                }
+            }
+            return result
         }
+
+
+        function difference(array,[values]){
+            var result = []
+            for(var i = 0 ; i <= array.length - 1 ; i++){
+                for(var j = 0 ; j <= values.length - 1 ; j++){
+                    if(array[i] == values[j]){
+                        result = array.splice(i,1)
+                    }
+                }
+            }
+            return result
+        }
+
+
+
+        function drop(array,n = 1){
+            array.splice(0,n)
+            return array
+        }
+
+
+
+        function dropright(array,n = 1){
+            var result= []
+            for(var i = 0;i < array.length - n ; i++){
+                result.push(array[i])
+            }
+            return result
+        }
+
+
+
+        function fill(array,value,start=0,end=array.length){
+            for(var i = start;i < end;i++){
+                array[i] = value
+            }
+            return array
+        }
+
+
+        function filter(){
+
+        }
+
+
+
+        function indexof(array,value,fromindex = 0){
+            for(var i = fromindex; i < array.length;i++){
+                if(array[i] == value){
+                    return i
+                }
+                return -1
+            }
+        }
+
+
+
+
         return{
             chunk,
             compact,
+            concat,
+            difference,
+            drop,
+            dropright,
+            fill,
+            filter,
+            indexof,
         }
     }()
