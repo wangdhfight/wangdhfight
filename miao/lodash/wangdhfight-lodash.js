19fc032001dd29fbef9a235b682fc351cafc5013
王丁辉 <1534212@qq.com>
2020-12-14 14:40:43 +0800

    提交

--- miao/lodash/wangdhfight-lodash.js
+++ miao/lodash/wangdhfight-lodash.js
@@ -107,46 +107,137 @@
                 return -1
             }
         }
-        function findLastIndex(){
-
-        }
-        function flatten(){
-
-        }
-        function flattenDeep(){
-
-        }
-        function flattenDepth(){
-
-        }
-        function fromPairs(){
-
-        }
-        function head(){
-
-        }
-        function inital(){
-
-        }
-        function indexOf(array,value,fromindex = 0){
-            for(var i = fromindex; i < array.length;i++){
-                if(array[i] == value){
-                    return i
+        function findLastIndex(array, f, fromIndex = array.length -1 ){
+            if (typeof(f) == 'function') {
+                for (let i = fromIndex; i < array.length; i++) {
+                    if (f(array[i])) return i
+                }
+                return -1
+            } else if (Array.isArray(f)) {
+                for (let i = fromIndex; i < array.length; i++) {
+                    for (let j in array[i]) {
+                        if (f[0] == j && f[1] == array[i][j]) return i
+                    }
+                }
+                return -1
+            } else if (typeof(f) == 'object') {
+                for (let i = fromIndex; i < array.length; i++) {
+                    for (let j in array[i]) {
+                        if (f[j] && array[i][j] == f[j]) return i
+                    }
+                }
+                return -1
+            } else if (typeof(f) == 'string') {
+                for (let i = fromIndex; i < array.length; i++) {
+                    for (let j in array[i]) {
+                        if (array[i][f]) return i
+                    }
                 }
                 return -1
             }
         }
+        function flatten(ary){
+            let result = []
+            for(let x of ary){
+                if(Array.isArray(x)){
+                    for(let i = 0 ; i < x.length; i++){
+                        result.push(x[i])
+                    }
+                }
+                else{
+                    result.push(x)
+                }
+            }
+            return result
+        }
+        function flattenDeep(ary,result = []){
+            for(var x of ary){
+                if(Array.isArray(x)){
+                    flattenDeep(x,result)
+                }
+                else{
+                    result.push(x)
+                }
+            }
+            return result
+        }
 
-        function reverse(){
+        function flattenDepth(ary,depth = 1){
+            if(depth === 0){
+                return ary
+            }
+            let res = []
+            ary.forEach((x) => {
+                if(Array.isArray(x)){
+                    res  = res.concat(x)
+                }
+                else{
+                    res.push(x)
+                }
+            })
+            return res
+        }
 
+        function fromPairs(ary){   
+                let res = {};
+                for (let i = 0; i < ary.length; i++) {
+                    res[ary[i][0]] = ary[i][1];
+                }
+                return res;
+        }
+
+        function head(ary){
+            if(ary == []){
+                return undefined
+            }
+            return ary.shift()
+        }
+
+        function inital(ary){
+            let n = ary.length - 1
+            return ary.slice(0,n)
+        }
+
+        function indexOf(array,value,idx = 0){
+            for(var i = idx; i < array.length ; i++){
+                if(array[i] === value) return i;
+                return -1
+            }
+        }
+
+        function reverse(ary){
+            function swap(ary,i,j){
+                var temp = ary [i]
+                ary[i] = ary [j]
+                ary[j] = temp
+            }
+            for(let i = 0,n = ary.length - i - 1; i < Math.floor(ary.length >> 1); i++){
+                swap(ary,i,n)
+            }
+            return ary
         }
         
-        function sortedIndex(){
-
+        function sortedIndex(array, value) {
+            for (i = 0; i < array.length; i++) {
+                if (array[i] >= value) {
+                    return i
+                }
+            }
+            return array.length;
         }
-        function every(){
 
+        function every(arr, boolean) {
+            if (boolean != Boolean) {
+                if (findIndex(arr, boolean) != -1) return true
+                return false
+            }
+        
+            for (e of arr) {
+                if (typeof e != "boolean") return false
+            }
+            return true
         }
+
         function find(array,f,fromIndex=0){
             if (typeof(f) == 'function') {
                 for (let i = fromIndex; i < array.length; i++) {
@@ -179,8 +270,20 @@
         function filter(){
 
         }
-        function toArray(){
-
+        function toArray(val){
+            var res = []
+            if(typeof(val) == 'object'){
+                for (let x in val) {
+                    res.push(val[x])
+                }
+            }
+            if(typeof(val) == 'string'){
+                return val.split('')
+            }
+            if(typeof(val) == 'number' || 'null'){
+                res = []
+            }
+            return res
         }
         function max(){
 
@@ -212,5 +315,16 @@
             indexOf,
             find,
             findIndex,
+            findLastIndex,
+            flatten,
+            flattenDeep,
+            flattenDepth,
+            fromPairs,
+            head,
+            inital,
+            reverse,
+            sortedIndex,
+            every,
+            toArray,
         }
     }()
